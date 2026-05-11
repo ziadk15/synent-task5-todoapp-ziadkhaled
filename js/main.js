8fd741fd40750f301959b6adee69cae1baa74d75
Ziad <ziadk9897@gmail.com>
2026-05-12 01:33:00 +0300

    Delete + Complete

--- js/main.js
+++ js/main.js
@@ -1,40 +1,48 @@
+import { getTasks, saveTasks } from "./storage.js";
 import { renderTasks } from "./ui.js";
 
-let tasks = [];
+let tasks = getTasks();
 
 const input = document.getElementById("taskInput");
-
 const btn = document.getElementById("addBtn");
-
 const container = document.getElementById("tasksContainer");
 
-
 function render() {
-
     renderTasks(tasks, container);
-
 }
-
 
 btn.onclick = () => {
 
     if (!input.value) return;
 
     tasks.push({
-
         id: Date.now(),
-
         title: input.value,
-
         completed: false
-
     });
 
     input.value = "";
 
+    saveTasks(tasks);
     render();
-
 };
 
+container.addEventListener("click", (e) => {
+
+    const id = Number(e.target.dataset.id);
+
+    if (e.target.classList.contains("delete")) {
+        tasks = tasks.filter(t => t.id !== id);
+    }
+
+    if (e.target.type === "checkbox") {
+        tasks = tasks.map(t =>
+            t.id === id ? { ...t, completed: !t.completed } : t
+        );
+    }
+
+    saveTasks(tasks);
+    render();
+});
 
 render();
--- js/ui.js
+++ js/ui.js
@@ -3,18 +3,14 @@ export const renderTasks = (tasks, container) => {
     container.innerHTML = "";
 
     tasks.forEach(task => {
-
         container.innerHTML += `
-        
             <div class="task">
-
+                <input type="checkbox" data-id="${task.id}" ${task.completed ? "checked" : ""}>
                 <span>${task.title}</span>
-
+                <button class="delete" data-id="${task.id}">X</button>
             </div>
-        
         `;
     });
-
 };
 
 
